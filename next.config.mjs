6cf76b582f4578a869e71abd7997b19aa3cddc08
reale <minhdan190904@gmail.com>
2026-06-14 16:50:26 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['rebrowser-playwright-core', 'ghost-cursor-playwright', '@2captcha/captcha-solver'],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(ttf|html)$/i,
